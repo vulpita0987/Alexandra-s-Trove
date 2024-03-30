cd857e403aa9e45b69dfc2fb14a676a9879fee67
vulpita0987 <bucur_ioana_alexandra@yahoo.com>
2024-03-30 19:19:16 +0000

    Comments done

--- Alexandra's Trove/Alexandra's Trove/Form13.cs
+++ Alexandra's Trove/Alexandra's Trove/Form13.cs
@@ -21,8 +21,10 @@ namespace Alexandra_s_Trove
             InitializeComponent();
         }
 
-        private async void OrdersPage_Load(object sender, EventArgs e)
+        private async void OrdersPage_Load(object sender, EventArgs e)//when form loads run this
         {
+            //if user is not logged in then adjust form accordingly
+            //users that are not logged in should not be able to access this form
             string clientId = ClientAccountAccess.GetID();
             if (clientId == "C0")
             {
@@ -30,13 +32,13 @@ namespace Alexandra_s_Trove
 
             }
 
-
+            //add product names from database to the search bar
             Dictionary<string, string> GetNames = new Dictionary<string, string>();
             GetNames = ProductHandling.ReturnProductNamesBasedOnIDs();
-          
             for (int i = 0; i < GetNames.Count; i++)
             { cboxSearchBar.Items.Insert(0, GetNames.ElementAt(i).Value); }
 
+            //get customer ID and connect to the database
             string CustomerID = ClientAccountAccess.GetID();
             string ConnectionString = "mongodb+srv://IoanaBucur:DGUEYGPUScania11bia@atlascluster.kuxwwx2.mongodb.net/?retryWrites=true&w=majority";
             string DatabaseName = "Assignment";
@@ -47,6 +49,7 @@ namespace Alexandra_s_Trove
 
             var data = await Coll.FindAsync(_ => true);
 
+            //create lists to store the data
             List<string> OrderIDs = new List<string>();
             List<string> ClientIDs = new List<string>();
             List<string> ProductsIDs = new List<string>();
@@ -62,7 +65,7 @@ namespace Alexandra_s_Trove
             {
                 if (data != null)
                 {
-                    //all orders
+                    //store all orders in lists
                     OrderIDs.Add(datas.ID);
                     ClientIDs.Add(datas.ClientID);
                     ProductsIDs.Add(datas.ProductIDs);
@@ -74,7 +77,7 @@ namespace Alexandra_s_Trove
                 }
             }
 
-            //orders for specific Customer ID
+            //lists for orders for specific Customer ID
             List<string> OrderIDs1 = new List<string>();
             List<string> ClientIDs1 = new List<string>();
             List<string> ProductsIDs1 = new List<string>();
@@ -85,7 +88,7 @@ namespace Alexandra_s_Trove
 
             for (int i = 0; i < OrderIDs.Count; i++)
             {
-                if (ClientIDs[i] == CustomerID)
+                if (ClientIDs[i] == CustomerID)//if the ID used for the orders match the id of the customer then store them in those lists
                 {
                     OrderIDs1.Add(OrderIDs[i]);
                     ClientIDs1.Add(ClientIDs[i]);
@@ -98,17 +101,18 @@ namespace Alexandra_s_Trove
 
             }
 
-            if (OrderIDs1.Count > 0) 
+            if (OrderIDs1.Count > 0) //if orders are accociated with the account then
             {
-                for (int i = 0; i < OrderIDs1.Count; i++) 
+                for (int i = 0; i < OrderIDs1.Count; i++) //go over each order associated with the account
                 {
 
-                    //MessageBox.Show(ProductsIDs1[i]);
+                    
 
                     string products = ProductsIDs1[i];
-                    int count = CountStringOccurrences(products, " ") + 1;
+                    int count = CountStringOccurrences(products, " ") + 1;//count space occurances
                     List<string> ProductsIDs2 = new List<string>();
                     
+                    //split ids
                     string[] ids = products.Split(' ');
 
                     for (int j = 0; j < ids.Length; j++)
@@ -116,10 +120,9 @@ namespace Alexandra_s_Trove
                         ProductsIDs2.Add(ids[j]);//ids in list - each as their own element
 
                     }
-                    //MessageBox.Show(string.Join("* ", ProductsIDs2));
-
-                    Dictionary<string, int> CountIDs = new Dictionary<string, int>();
                     
+                    //count number of product IDs (each independantly)
+                    Dictionary<string, int> CountIDs = new Dictionary<string, int>();
                     
                     CountIDs = ProductHandling.CountHowManyProductsBasedOnIDS(ProductsIDs2);
                     //MessageBox.Show(string.Join("* ", ProductsIDs2);
@@ -129,8 +132,8 @@ namespace Alexandra_s_Trove
                          Console.WriteLine("Key: {0}, Value: {1}",
                              element.Key, element.Value);
                      }*/
-                    
 
+                    //show product and how many - then move on to next line 
                     string productNamesHowMany = "Products bought: ";
                     for (int j = 0; j < CountIDs.Count ; j++)
                     {
@@ -143,16 +146,15 @@ namespace Alexandra_s_Trove
                             
                     }
 
+                    
                     double total = Convert.ToDouble(Totals1[i]) + Convert.ToDouble(DeliveryPrices1[i]);
                     rtbMain.AppendText("Order ID: " + OrderIDs1[i] + ". Total:£" + total.ToString() + ". Delivered On: " 
                         + EstimatedDeliveries1[i] + ". "+ productNamesHowMany);
                     rtbMain.AppendText(".\n");
 
 
-                    //show product and how many - then move on to next line 
-                    //finally - make it look tidy
-
-                    //Console.WriteLine("\n\n");
+                   
+                    
 
 
 
@@ -160,7 +162,7 @@ namespace Alexandra_s_Trove
 
             }
             else 
-            {
+            {//if no orders are associated with the account then display message for user
                 rtbMain.Text = "No Orders Have Been Placed Yet";
             }
 
@@ -169,19 +171,20 @@ namespace Alexandra_s_Trove
 
         public static int CountStringOccurrences(string stringToCountFrom, string stringToFind)
         {
-            // Loop through all instances of the string 'text'.
+            // count occurances of specific string (stringToFind)
             int stringCount = 0;
             int i = 0;
-            while ((i = stringToCountFrom.IndexOf(stringToFind, i)) != -1)
+            while ((i = stringToCountFrom.IndexOf(stringToFind, i)) != -1)//check that there is text after the last found stringToFind
             {
-                i += stringToFind.Length;
-                stringCount++;
+                i += stringToFind.Length;//ignore portion of string found before stringToFind of this iteration
+                stringCount++;//increase count
             }
-            return stringCount;
+            return stringCount;//return number of occurances
         }
 
         private async void picSearchLoop_Click(object sender, EventArgs e)
         {
+            //connect to database
             string ConnectionString = "mongodb+srv://IoanaBucur:DGUEYGPUScania11bia@atlascluster.kuxwwx2.mongodb.net/?retryWrites=true&w=majority";
             string DatabaseName = "Assignment";
             string CollectionName = "Product";
@@ -214,10 +217,11 @@ namespace Alexandra_s_Trove
             bool productExists = false;
             for (int i = 0; i < Names.Count; i++)
             {
-                if (Names[i] == product)
+                if (Names[i] == product)//if product name from database matches product name inputted by user then 
                 {
+                    //set ID of for ProductHandling
                     ProductHandling.SetID(ProductIDs[i]);
-
+                    //open product form and close current form
                     ProductPage pp = new ProductPage(); pp.Show();
                     productExists = true;
                     Hide();
@@ -226,7 +230,8 @@ namespace Alexandra_s_Trove
                 }
             }
             if (productExists == false)
-            {
+            {//if the inputted name does not match any product name form the database
+             //then display message and refresh search bar
                 MessageBox.Show("Product Does NOT Exist. Please Try Again");
                 cboxSearchBar.Text = "";
             }
@@ -234,6 +239,7 @@ namespace Alexandra_s_Trove
 
         private void lblVegetables_Click(object sender, EventArgs e)
         {
+            //show category form depending on text of the label
             string chategory = lblVegetables.Text;
             ChategoryHandling.SetChategory(chategory);
 
@@ -244,6 +250,7 @@ namespace Alexandra_s_Trove
 
         private void lblFruits_Click(object sender, EventArgs e)
         {
+            //show category form depending on text of the label
             string chategory = lblFruits.Text;
             ChategoryHandling.SetChategory(chategory);
 
@@ -254,6 +261,7 @@ namespace Alexandra_s_Trove
 
         private void lblDesserts_Click(object sender, EventArgs e)
         {
+            //show category form depending on text of the label
             string chategory = lblDesserts.Text;
             ChategoryHandling.SetChategory(chategory);
 
@@ -264,47 +272,53 @@ namespace Alexandra_s_Trove
 
         private void picAlex_Click(object sender, EventArgs e)
         {
+            //depending on user ID - display main guest/signed in form
             string clientId = ClientAccountAccess.GetID();
             if (clientId == "C0")
             {
                 GuestPage lip = new GuestPage(); lip.Show();
-                //Form.Close();
-                Hide();// rp = new RegisterPage(); rp.Close();#
+                
+                Hide();
             }
             else
             {
                 LoggedInPage lip = new LoggedInPage(); lip.Show();
-                //Form.Close();
-                Hide();// rp = new RegisterPage(); rp.Close();#
+                
+                Hide();
             }
 
         }
 
         private void picTrove_Click(object sender, EventArgs e)
         {
+
+            //depending on user ID - display main guest/signed in form
+
             string clientId = ClientAccountAccess.GetID();
             if (clientId == "C0")
             {
                 GuestPage lip = new GuestPage(); lip.Show();
-                //Form.Close();
-                Hide();// rp = new RegisterPage(); rp.Close();#
+                
+                Hide();
             }
             else
             {
                 LoggedInPage lip = new LoggedInPage(); lip.Show();
-                //Form.Close();
-                Hide();// rp = new RegisterPage(); rp.Close();#
+                
+                Hide();
             }
 
         }
 
         private void lblFeedbackSurvey_Click(object sender, EventArgs e)
         {
+            //show feedback surbey
             FeedbackSurveyPage feedbackSurveyPage = new FeedbackSurveyPage(); feedbackSurveyPage.Show();
         }
 
         private void lblTermsConditions_Click(object sender, EventArgs e)
         {
+            //display terms and conditions
             MessageBox.Show("Terms List\r\n1. ‘Terms’ refers to the rules listed in this agreement.\r\n2. ‘Contract’ refers to the entirety of this agreement.\r\n3. ‘Client’ refers to the buyer/customer/purchaser of goods/products through this shopping platform. \r\n4. ‘Product’ refers to each of the items/foods advertised on this shopping platform.\r\n5. ‘Seller’ refers to the individual/individuals that provides/provide the goods/products and advertises/sells them using this shopping platform. \r\n6. ’Company’ refers to the organisation that created the shopping platform. The company is also managing the shopping platform.\r\nThe ‘Company’ and ‘Seller’ exist as the same legal entity – they are differentiated by these two terms in this contract for clarity.\r\nTerms\r\nBy agreeing to this contract, the client understands that their information will be stored and used to improve the shopping platform therefore improving their shopping experience. \r\nThe company will strictly use the collected client information to improve the shopping platform. \r\nThe company will appropriately dispose of the client's information if the client decides to delete their account.\r\nThe company will not store any information of users that do not create accounts.\r\nThe client information will be disposed of in less than 1 year after the account closure. *The company may store the client’s information for longer if required by law/the authorities.\r\nClients understand that foul language will not be tolerated by the company. If any foul language is used by the client, the company may close the account. \r\nThe company will not use foul language. \r\nThe company will not share the client's information with any third parties unless required to do so by the client or because of legal reasons. \r\nThe client agrees to keep their password private for account security reasons.\r\nThe client understands that the seller does not take responsibility for any allergic reactions that the client might have to the products they purchase. \r\nThe client takes it upon themselves to read the specifications of the products and choose products that suit their specific needs/requirements.\r\nThe client agrees to never use the shopping platform for malicious purposes. If the company has reasonable beliefs that the client is using or has used to platform for malicious purposes, the company may report the client to the authorities or/and terminate the account.\r\nThe client understands that they may not resell the products purchased using this shopping platform. If the client resells/attempts to resell products that have been purchased using this platform, then the company may take legal action against them and/or terminate their account.\r\nThe client agrees to not use any of the content that is owned by the company. The content includes but is not limited to product pictures, product descriptions, company logos, company icons, and the company name.\r\nBy ticking the box, the client confirms that they have read and understood the terms and conditions of this contract.\r\n", "Terms And Conditions");
 
 
@@ -312,11 +326,14 @@ namespace Alexandra_s_Trove
 
         private void picBasket_Click(object sender, EventArgs e)
         {
+            //display basket form and hide this form
             BasketPage basket = new BasketPage(); basket.Show(); Hide();
         }
 
         private void lblAccount_Click(object sender, EventArgs e)
         {
+            //depending on user ID - display main account/registration form
+
             string clientId = ClientAccountAccess.GetID();
             if (clientId == "C0")
             {
@@ -331,7 +348,7 @@ namespace Alexandra_s_Trove
         }
 
         private void lblOrders_Click(object sender, EventArgs e)
-        {
+        {//show orders page and hide this page
             OrdersPage ordersPage = new OrdersPage(); ordersPage.Show(); Hide();
         }
     }
--- Alexandra's Trove/Alexandra's Trove/Form14.cs
+++ Alexandra's Trove/Alexandra's Trove/Form14.cs
@@ -20,20 +20,25 @@ namespace Alexandra_s_Trove
 
         private void lblAddReview_Click(object sender, EventArgs e)
         {
+            //check if rich text boxes are empty
+            //if they are not empty then
             if (rtboxQ1.Text != "" && rtboxQ2.Text != "" && rtboxQ3.Text != "" && rtboxQ4.Text != "" && rtboxQ5.Text != "")
             {
+                //get id of user and data from the boxes
                 string ID = ClientAccountAccess.GetID();
                 string Q1 = rtboxQ1.Text;
                 string Q2 = rtboxQ2.Text;
                 string Q3 = rtboxQ3.Text;
                 string Q4 = rtboxQ4.Text;
                 string Q5 = rtboxQ5.Text;
+                //insert new application review into the database
                 DatabaseHandler.InsertNewApplicationReview(ID, Q1, Q2, Q3, Q4, Q5);
-                MessageBox.Show("Your Review has been Added");
-                Hide();
+                MessageBox.Show("Your Review has been Added");//display confirmation message for user
+                Hide();//hide this form
             }
             else
             {
+                //if any rich text box is empty then display this message for the user
                 MessageBox.Show("All Questions Must be Answered to Create the Review");
             }
         }
